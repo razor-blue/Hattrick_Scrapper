b076e5692cf38f9a49d09fa9441777371edfc509
razor-blue <razorblue@interia.pl>
2023-10-23 14:34:57 +0200

    Polska db update

--- src/test/scala/YouthDatabase.scala
+++ src/test/scala/YouthDatabase.scala
@@ -1082,7 +1082,7 @@ object run extends App{
   //new YouthAnalysis("test-TL'a")
   //new YouthAnalysis(678445)
   //new YouthAnalysis(2955119)
-  //new YouthAnalysis("Polska")
+  new YouthAnalysis("Polska")
   //new YouthAnalysis("Kenia")
   //new YouthAnalysis("Ligi_1-4")
   //new YouthAnalysis("5 Liga")
